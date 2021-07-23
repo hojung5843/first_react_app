2e3c3007b41fb18479db758bfacaa3a8ee279faf
Hojung Park <hojung7737@gmail.com>
2021-07-23 15:27:29 -0700

    delete func added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: this.props.value
+        value: this.props.value
     };
 
-    handleIncrement = product => {
-        console.log(product);
-        this.setState({ count: this.state.count + 1 })
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 })
     }
 
     render() { 
-        console.log('props', this.props)
-        return <React.Fragment>
+        return <div>
                     <span className = {this.getBadgeClasses()}>{this.formatCount()}</span>
                     <button onClick = {this.handleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
+                    <button onClick = {() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
                     <br />
-                </React.Fragment>;
+                </div>;
     }
 
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count }  = this.state;
+        const { value: count }  = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,14 +5,31 @@ class Counters extends Component {
     state = { 
         counters: [ 
             {id: 1, value: 0},
-            {id: 2, value: 0},
+            {id: 2, value: 2},
             {id: 3, value: 0},
             {id: 4, value: 0}
         ]
-     }
+    }
+
+    handleDelete = (counterId) =>{
+        console.log(counterId)
+        const counters =  this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters})
+    }
     render() { 
-        return ( this.state.counters.map(counter => 
-        <Counter key = {counter.id} value = {counter.value} selected = {true} />));
+        return ( 
+            <div>
+                {this.state.counters.map(counter => (
+                    <Counter 
+                        key = {counter.id} 
+                        onDelete = {this.handleDelete} 
+                        value = {counter.value} 
+                        id = {counter.id}>
+                    </Counter>
+                ))}
+                
+            </div>    
+        );
     }
 }
  
