19f7798669406ac85193314d85999d81037974a3
Hojung Park <hojung7737@gmail.com>
2021-07-23 14:43:52 -0700

    revised conters.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: this.props.value
     };
 
     handleIncrement = product => {
@@ -11,9 +11,11 @@ class Counter extends Component {
     }
 
     render() { 
+        console.log('props', this.props)
         return <React.Fragment>
                     <span className = {this.getBadgeClasses()}>{this.formatCount()}</span>
-                    <button onClick = { () => this.handleIncrement(product)} className = "btn btn-secondary btn-sm">Increment</button>
+                    <button onClick = {this.handleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
+                    <br />
                 </React.Fragment>;
     }
 
