6b35dc1e9305f42bf778f709ec08bf5680a77440
Hojung Park <hojung7737@gmail.com>
2021-07-24 12:57:58 -0700

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App'
-import Counters from './components/counters';
 const element = <h1>Hello World</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
